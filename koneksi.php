16f7b3d57de4e09c065b28d92812f6fe26ce890b
ariskadara <ariskabudu976@gmail.com>
2025-05-25 18:27:08 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,12 @@
+Ko<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "webku";
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+if (!$conn) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+?>

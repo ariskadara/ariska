22733e5a4a5ea8e5e527a39468ed92120ff81e14
ariskadara <ariskabudu976@gmail.com>
2025-09-09 15:06:31 +0700

    Add files via upload

---
+++ Table_dosen(1).sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 09, 2025 at 04:53 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `data_dosen`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Table_dosen`
+--
+
+CREATE TABLE `table_dosen` (
+  `Nisn` int(11) NOT NULL,
+  `Nama` int(11) NOT NULL,
+  `alamt` int(11) NOT NULL,
+  `Jurusan` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Table_dosen`
+--
+ALTER TABLE `Table_dosen`
+  ADD PRIMARY KEY (`alamt`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
